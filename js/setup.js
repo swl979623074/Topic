e0d913dae7faf2014bea4aefa68fc8117cb1d76e
swl979623074 <979623074@qq.com>
2017-02-01 20:59:16 +0800

    添加设置模块，分为修改密码、系统说明、等级制度

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,3 @@
-
 (function(){
 	window.onresize = function() {
 		throttle(window.onload)
@@ -16,3 +15,30 @@
 	    html.style.fontSize = window.innerWidth / 100 + "px";
 	}
 })();
+
+$(function(){
+	$("div").click(function(event){
+		var type = event.target.id;
+		var url = getUrl(type);
+		if(url != null){
+			toggleClass(event.target);
+			$("#changUrl").attr('src',url);
+		}
+	})
+	
+	function getUrl(type){
+		var url = null;
+		switch(type){
+			case "summary":url='./summary.html';break;
+			case "pwd":url='./pwd.html';break;
+			case "grade":url='./grade.html';break;
+			default:break;
+		}
+		return url;
+	}
+	
+	function toggleClass(ele){
+		$(".menu_select").removeClass("menu_select");
+		$(ele).addClass("menu_select")
+	}
+})
